d5f87212fa334a3466c4f5ea563fcc3150a97e9e
risu729 <git@risunosu.com>
2024-06-06 20:20:48 +0900

    feat(win/install.ps1,wsl/install.sh): setup git config after brower installation

--- win/install.ps1
+++ win/install.ps1
@@ -4,10 +4,14 @@ wsl --install --distribution $distribution
 wsl --set-default   $distribution
 
 # ref: https://github.com/microsoft/WSL/issues/3284
-wsl /usr/bin/env bash -c "bash <(curl -fsSL https://dot.risunosu.com/wsl)"
+wsl /usr/bin/env bash -c "export SKIP_GIT_SETUP=true; bash <(curl -fsSL https://dot.risunosu.com/wsl)"
 
 $wsl_username = "$(wsl whoami)"
 winget import --import-file "\\wsl.localhost\$distribution\home\$wsl_username\github\dotfiles\win\winget.json" --disable-interactivity --accept-package-agreements
+
+# setup git config after brower is installed
+# use -i, interactive mode, to load .bashrc
+wsl /usr/bin/env bash -ic "~/github/dotfiles/wsl/setup-git.sh"
 
 # cspell:ignore powertoys
 # Set PowerToys settings backup directory
