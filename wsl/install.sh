fda9e649f52036d47f30c025618fb0ebb8f09b8e
risu729 <git@risunosu.com>
2024-06-07 15:48:17 +0900

    fix(wsl/install.sh): fix condition to run setup-git.sh

--- wsl/install.sh
+++ wsl/install.sh
@@ -66,6 +66,6 @@ echo installed mise
 echo installed dotfiles!
 
 # shellcheck disable=SC2154 # CI is defined in GitHub Actions, SKIP_GIT_SETUP may be defined as environment variable
-if [[ ${CI} != true || ${SKIP_GIT_SETUP} != true ]]; then
+if [[ ${CI} != true && ${SKIP_GIT_SETUP} != true ]]; then
 	~/github/dotfiles/wsl/setup-git.sh
 fi
