e9ee63040af4982f39cccde7a9900af0faffc3c5
Risu <79110363+risu729@users.noreply.github.com>
2024-09-21 18:27:39 +0900

    fix(win/install.ps1): set powertoys registry before setting up git config (#468)

--- win/install.ps1
+++ win/install.ps1
@@ -17,13 +17,13 @@ wsl /usr/bin/env bash -c "SKIP_GIT_SETUP=true bash <(curl -fsSL $wsl_script)"
 $wsl_username = "$(wsl whoami)"
 winget import --import-file "\\wsl.localhost\$distribution\home\$wsl_username\github\dotfiles\win\winget.json" --disable-interactivity --accept-package-agreements
 
-# setup git config after browser is installed
-# use -i, interactive mode, to load .bashrc
-wsl /usr/bin/env bash -ic "~/github/dotfiles/wsl/setup-git.sh"
-
 # cspell:ignore powertoys
 # Set PowerToys settings backup directory
 # ref: https://github.com/microsoft/PowerToys/blob/29ce15bb8a8b6496fb55e38ec72f746a3a4f9afa/src/settings-ui/Settings.UI.Library/SettingsBackupAndRestoreUtils.cs#L391
 $powertoys_backup_dir = "\\wsl.localhost\$distribution\home\$wsl_username\github\dotfiles\win\powertoys"
 # cspell:ignore hkcu
 Set-ItemProperty -Path HKCU:Software\Microsoft\PowerToys -Name SettingsBackupAndRestoreDir -Value "$powertoys_backup_dir"
+
+# setup git config after browser is installed
+# use -i, interactive mode, to load .bashrc
+wsl /usr/bin/env bash -ic "~/github/dotfiles/wsl/setup-git.sh"
