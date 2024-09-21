690eb8be96dd22148b6e822bcf2f8c8cdbe3f77c
risu729 <git@risunosu.com>
2024-09-21 18:50:45 +0900

    fix(wsl/install.sh): remove test codes

--- wsl/install.sh
+++ wsl/install.sh
@@ -75,21 +75,6 @@ eval "${brew_env}"
 brew bundle install --global --no-lock
 echo installed Homebrew
 
-# specifying the source file is not supported by vscode extension
-# ref: https://github.com/vscode-shellcheck/vscode-shellcheck/issues/19
-# Homebrew
-brew_env="$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-eval "${brew_env}"
-
-# mise
-mise_activate="$(mise activate bash)"
-eval "${mise_activate}"
-alias mr="mise run"
-
-# mise
-mise_shims="$(mise activate bash --shims)"
-eval "${mise_shims}"
-
 # cspell:ignore reshim
 # mise reshim is required to avoid "No such file or directory" error
 # ref: https://github.com/jdx/mise/issues/2260
