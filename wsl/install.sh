8e839c6a37d03f50f43fab90e36fd61303f3bcef
risu729 <git@risunosu.com>
2024-09-21 15:58:40 +0900

    fix(wsl/install.sh): remove mise related commands for test

--- wsl/install.sh
+++ wsl/install.sh
@@ -79,21 +79,12 @@ echo installed Homebrew
 brew_env="$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "${brew_env}"
 
-# mise-en-place
-mise_activate="$(mise activate bash)"
-eval "${mise_activate}"
-alias mr="mise run"
-
 # VS Code
 alias code="code-insiders"
 
 # gpg
 GPG_TTY=$(tty)
 export GPG_TTY
-
-# mise-en-place
-mise_shims="$(mise activate bash --shims)"
-eval "${mise_shims}"
 
 # cspell:ignore reshim
 # mise reshim is required to avoid "No such file or directory" error
