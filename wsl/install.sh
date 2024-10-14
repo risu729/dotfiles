9c140a4ee036804965e6eafcf101d5d157e22fe6
risu729 <git@risunosu.com>
2024-10-14 15:14:43 +0900

    Revert "fix(wsl/install.sh): activate mise shims"
    This reverts commit 5518e425258e246087219be180319529d67600d9.

--- wsl/install.sh
+++ wsl/install.sh
@@ -63,8 +63,6 @@ eval "${brew_env}"
 brew bundle install --global --no-lock
 echo installed Homebrew
 
-# shellcheck disable=SC1090 # vs code extension doesn't support source
-source ~/.bashrc
 mise install --yes
 echo installed mise
 
