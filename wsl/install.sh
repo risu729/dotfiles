022185203fc1f1992d173f7f048f01a62530ff00
Risu <79110363+risu729@users.noreply.github.com>
2024-10-21 19:47:54 +0900

    fix(wsl/install.sh): install mise

--- wsl/install.sh
+++ wsl/install.sh
@@ -22,6 +22,14 @@ sudo add-apt-repository ppa:wslutilities/wslu
 sudo apt-get update
 sudo apt-get install --yes wslu
 
+# install mise
+# ref: https://mise.jdx.dev/getting-started.html#apt
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt-get update
+sudo apt-get install -y mise
+
 mkdir --parents ~/github
 cd ~/github
 
