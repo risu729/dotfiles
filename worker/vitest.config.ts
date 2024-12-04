185bd3ae962a44d8bc5c455cc08ced75a333f6d3
Risu <79110363+risu729@users.noreply.github.com>
2024-12-04 22:30:06 +0900

    chore(worker/vitest.config.ts): get compatibility from bun pm ls

--- worker/vitest.config.ts
+++ worker/vitest.config.ts
@@ -1,21 +1,10 @@
 // ref: https://vitest.dev/config/
 
-import { readFile } from "node:fs/promises";
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { $ } from "execa";
 
-const wranglerToml = await readFile("./wrangler.toml", "utf-8");
-
-// cspell:ignore workerd
-// extract the compatibility_date from the compatibility_workerd_version in wrangler.toml;
-// ref: github.com/cloudflare/workers-sdk/blob/81dfb1746a2b2a17c06f809b2da9f937810ca701/packages/create-cloudflare/src/helpers/compatDate.ts#L27-L28
-const compatibilityDateRegex =
-	/# *compatibility_workerd_version *= *"\d+\.(\d{4})(\d{2})(\d{2})\.\d+"/;
-const compatibilityDateMatch = wranglerToml.match(compatibilityDateRegex);
-if (!compatibilityDateMatch) {
-	throw new Error(
-		"Could not find compatibility_workerd_version in wrangler.toml",
-	);
-}
+const { stdout: compatibilityDate } =
+	await $`mise run worker:wrangler-compat-date`;
 
 export default defineWorkersConfig({
 	test: {
@@ -28,7 +17,7 @@ export default defineWorkersConfig({
 				// ref: https://github.com/cloudflare/workers-sdk/blob/e42f32071871b0208e9f00cfd7078d8a5c03fe38/packages/vitest-pool-workers/src/pool/config.ts#L208
 				// cspell:ignore miniflare
 				miniflare: {
-					compatibilityDate: `${compatibilityDateMatch[1]}-${compatibilityDateMatch[2]}-${compatibilityDateMatch[3]}`,
+					compatibilityDate: compatibilityDate,
 				},
 				singleWorker: true,
 			},
