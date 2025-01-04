c683ac204961a52d1b59f90ec1b481fe4c34287f
Risu <79110363+risu729@users.noreply.github.com>
2025-01-04 23:22:46 +0900

    chore(tasks.toml): use jschema-validator npm package

---
+++ jschema.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jschema-validator";
+
+const config: Config = {
+	"winget.json": {
+		// disable using flag `u
+		// schema includes a regex pattern with `\-` which is invalid (or unnecessary) escape
+		// invalid escape sequences are not allowed with flag `u`
+		// ref: https://tc39.es/archives/bugzilla/3157/
+		unicodeRegExp: false,
+	},
+};
+
+export default config;
