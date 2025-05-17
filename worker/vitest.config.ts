4ba848358e2e30a652fac1049a0804d4e9ec985d
Risu <79110363+risu729@users.noreply.github.com>
2025-05-18 07:23:47 +1000

    chore(worker/wrangler.jsonc): fix compatibility_date (#1777)

--- worker/vitest.config.ts
+++ worker/vitest.config.ts
@@ -3,9 +3,6 @@
 import { execSync } from "node:child_process";
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 
-const compatibilityDate = execSync("mise run worker:wrangler:compat-date")
-	.toString()
-	.trim();
 const latestCommitHash = execSync("git rev-parse HEAD").toString().trim();
 
 export default defineWorkersConfig({
@@ -18,12 +15,6 @@ export default defineWorkersConfig({
 			workers: {
 				wrangler: {
 					configPath: "./wrangler.jsonc",
-				},
-				// merged with wrangler.jsonc
-				// ref: https://github.com/cloudflare/workers-sdk/blob/e42f32071871b0208e9f00cfd7078d8a5c03fe38/packages/vitest-pool-workers/src/pool/config.ts#L208
-				// cspell:ignore miniflare
-				miniflare: {
-					compatibilityDate: compatibilityDate,
 				},
 				singleWorker: true,
 			},
