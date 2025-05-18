c06af873c8f654bf55915fa24f3d224bd176c41d
Risu <79110363+risu729@users.noreply.github.com>
2025-05-19 05:52:25 +1000

    chore(worker/): migrate to cloudflare vite plugin (#1784)
    * chore(worker/): migrate to cloudflare vite plugin
    
    * chore(worker/vitest.config.ts): split config from vite
    
    * chore(worker/tsconfig.*.json): fix tsconfig

---
+++ worker/vite.config.ts
@@ -0,0 +1,30 @@
+import { execSync } from "node:child_process";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from "vite";
+
+const remoteInfo = execSync("git remote show origin").toString();
+// in cloudflare workers builds, the url is in the format `https://*****@github.com//owner/repo`
+// not sure why there are two slashes, so use `+` to match one or more slashes
+const repoName = remoteInfo.match(
+	/Fetch URL:.*github\.com\/+(?<repo>[^/.]+\/[^/.]+)/,
+)?.groups?.["repo"];
+if (!repoName) {
+	throw new Error("Could not determine repository name from git remote.");
+}
+const defaultBranch = remoteInfo.match(/HEAD branch: (?<branch>.+)/)?.groups?.[
+	"branch"
+];
+if (!defaultBranch) {
+	throw new Error("Could not determine default branch from git remote.");
+}
+
+// ref: https://vite.dev/config/
+export default defineConfig({
+	define: {
+		// biome-ignore lint/style/useNamingConvention: constants
+		__REPO_NAME__: JSON.stringify(repoName),
+		// biome-ignore lint/style/useNamingConvention: constants
+		__DEFAULT_BRANCH__: JSON.stringify(defaultBranch),
+	},
+	plugins: [cloudflare()],
+});
--- worker/vitest.config.ts
+++ worker/vitest.config.ts
@@ -1,23 +1,37 @@
-// ref: https://vitest.dev/config/
-
 import { execSync } from "node:child_process";
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { mergeConfig } from "vitest/config";
+import viteConfig from "./vite.config";
 
 const latestCommitHash = execSync("git rev-parse HEAD").toString().trim();
+if (!latestCommitHash) {
+	throw new Error("Could not determine latest commit hash from git.");
+}
 
-export default defineWorkersConfig({
-	test: {
-		env: {
-			// biome-ignore lint/style/useNamingConvention: env var
-			LATEST_COMMIT_HASH: latestCommitHash,
+// ref: https://vitest.dev/config/
+export default mergeConfig(
+	viteConfig,
+	defineWorkersConfig({
+		// fix constants in tests
+		define: {
+			// biome-ignore lint/style/useNamingConvention: constants
+			__REPO_NAME__: JSON.stringify("risu729/dotfiles"),
+			// biome-ignore lint/style/useNamingConvention: constants
+			__DEFAULT_BRANCH__: JSON.stringify("main"),
 		},
-		poolOptions: {
-			workers: {
-				wrangler: {
-					configPath: "./wrangler.jsonc",
+		test: {
+			env: {
+				// biome-ignore lint/style/useNamingConvention: env var
+				LATEST_COMMIT_HASH: latestCommitHash,
+			},
+			poolOptions: {
+				workers: {
+					wrangler: {
+						configPath: "./wrangler.jsonc",
+					},
+					singleWorker: true,
 				},
-				singleWorker: true,
 			},
 		},
-	},
-});
+	}),
+);
