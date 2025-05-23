e3f75a79ae149a25b796cc11faf5ecddc3b72ddc
Risu <79110363+risu729@users.noreply.github.com>
2025-05-23 19:38:21 +1000

    build(worker/docker/entrypoint.sh): remove min_version from mise config to ignore

--- worker/docker/entrypoint.sh
+++ worker/docker/entrypoint.sh
@@ -52,5 +52,9 @@ echo "==> Checked out FETCH_HEAD (commit ${GIT_COMMIT_SHA})."
 
 echo "==> Successfully sparse-cloned and checked out commit ${GIT_COMMIT_SHA}."
 
+# remove min_version from mise.toml to ignore version check
+sed --in-place '/^min_version/d' mise.toml
+echo "==> Removed min_version from mise.toml." 
+
 # --host required to be accessible from other containers
 exec /bin/bash -c "mise run worker:preview --host"
