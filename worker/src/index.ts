95b3f35cc872a79b409924d3bbfe0cdcf6a8aee4
Risu <79110363+risu729@users.noreply.github.com>
2025-06-10 01:59:25 +1000

    chore(worker/): use wrangler types (#2002)
    * chore(worker/): use wrangler types
    
    * style: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -97,4 +97,5 @@ app.get("/:os{win|wsl}", async ({ req, text }) => {
 });
 
 // biome-ignore lint/style/noDefaultExport: required by hono
-export default app;
+// biome-ignore lint/correctness/noUndeclaredVariables: declared in worker-configuration.d.ts
+export default app satisfies ExportedHandler;
--- worker/test/env.d.ts
+++
@@ -1,9 +0,0 @@
-// biome-ignore lint/nursery/useImportRestrictions: for test
-import type { Env } from "../src/env.ts";
-
-type Writeable<T> = { -readonly [P in keyof T]: T[P] };
-
-declare module "cloudflare:test" {
-	// remove readonly from Env because we want to be able to modify it in tests
-	interface ProvidedEnv extends Writeable<Env> {}
-}
